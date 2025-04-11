475adea0e9ccc013c6b6952fd4fc920327acf90a
eunkang-choi <dmsrkd1216@gmail.com>
2025-04-11 18:16:15 +0900

    Feat: 유저 도메인 래포지토리 업데이트
    - UUID로 유저도메인 엔티티 조회 기능 추가

--- src/main/java/io/hhplus/concert/domain/user/repository/UserRepository.java
+++ src/main/java/io/hhplus/concert/domain/user/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package io.hhplus.concert.domain.user.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import io.hhplus.concert.domain.user.entity.User;
 
 public interface UserRepository {
     User save(User user);
     User findById(long id);
+    User findByUUID(UUID uuid);
 }
