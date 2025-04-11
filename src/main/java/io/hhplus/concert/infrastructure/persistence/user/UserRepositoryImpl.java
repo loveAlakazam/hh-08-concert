9a9b1241e7c99364c8e821597b14d40cd44f3c4d
eunkang-choi <dmsrkd1216@gmail.com>
2025-04-11 18:17:05 +0900

    Feat: 유저 인프라레이어 구현체 작성
    - UUID로 유저조회 쿼리문 작성
    - 유저 데이터베이스 구현체와 JPA 정의

--- src/main/java/io/hhplus/concert/infrastructure/persistence/user/UserRepositoryImpl.java
+++ src/main/java/io/hhplus/concert/infrastructure/persistence/user/UserRepositoryImpl.java
@@ -1,6 +1,8 @@
 package io.hhplus.concert.infrastructure.persistence.user;
 
 
+import java.util.UUID;
+
 import io.hhplus.concert.domain.common.exceptions.NotFoundException;
 import io.hhplus.concert.domain.user.entity.User;
 import io.hhplus.concert.domain.user.repository.UserRepository;
@@ -18,5 +20,10 @@ public class UserRepositoryImpl implements UserRepository {
     @Override
     public User findById(long id) {
         return  userJpaRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public User findByUUID(UUID uuid) {
+        return userJpaRepository.findByUUID(uuid).orElse(null);
     }
 }
