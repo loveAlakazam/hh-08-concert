1a985c7813dc7f4f66140592f29ce66712457dad
eunkang-choi <dmsrkd1216@gmail.com>
2025-04-11 18:18:42 +0900

    Feat: 토큰 도메인 인프라레이어 정의

--- src/main/java/io/hhplus/concert/infrastructure/persistence/token/TokenRepositoryImpl.java
+++ src/main/java/io/hhplus/concert/infrastructure/persistence/token/TokenRepositoryImpl.java
@@ -1,9 +1,29 @@
 package io.hhplus.concert.infrastructure.persistence.token;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import io.hhplus.concert.domain.token.entity.Token;
 import io.hhplus.concert.domain.token.repository.TokenRepository;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class TokenRepositoryImpl implements TokenRepository {
     private final TokenJpaRepository tokenJpaRepository;
+
+    @Override
+    public Token findOneByUUID(UUID uuid) {
+        return tokenJpaRepository.findOneByUUID(uuid).orElse(null);
+    }
+
+    @Override
+    public Token saveOrUpdate(Token token) {
+        return tokenJpaRepository.save(token);
+    }
+
+    @Override
+    public List<Token> findAllExpiredTokens() {
+        return tokenJpaRepository.findAllExpiredTokens(LocalDateTime.now());
+    }
 }
