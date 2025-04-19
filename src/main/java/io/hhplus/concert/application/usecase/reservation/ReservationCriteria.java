87586727685cda2c687a3f35246341e3af565484
eunkang-choi <dmsrkd1216@gmail.com>
2025-04-20 07:03:07 +0900

    Fix: ReservationCriteria 유효성검사 추가
    - userId <= 0 인경우 유효성검사 오류추가
    - concertSeatId <= 0 인경우 유효성검사 오류추가

--- src/main/java/io/hhplus/concert/application/usecase/reservation/ReservationCriteria.java
+++ src/main/java/io/hhplus/concert/application/usecase/reservation/ReservationCriteria.java
@@ -1,8 +1,15 @@
 package io.hhplus.concert.application.usecase.reservation;
 
+import static io.hhplus.concert.interfaces.api.user.CommonErrorCode.*;
+
+import io.hhplus.concert.interfaces.api.common.InvalidValidationException;
+
 public class ReservationCriteria {
 	public record ReserveConcertSeat(long userId, long concertSeatId) {
 		public static ReserveConcertSeat of(long userId, long concertSeatId) {
+
+			if( userId <= 0 ) throw new InvalidValidationException(ID_SHOULD_BE_POSITIVE_NUMBER);
+			if( concertSeatId <= 0 ) throw new InvalidValidationException(ID_SHOULD_BE_POSITIVE_NUMBER);
 			return new ReserveConcertSeat(userId, concertSeatId);
 		}
 	}
