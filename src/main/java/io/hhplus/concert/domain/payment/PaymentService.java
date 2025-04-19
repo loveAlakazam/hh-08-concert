bd0fdc71ff1bc13e9097d820884ae1548bf972b8
eunkang-choi <dmsrkd1216@gmail.com>
2025-04-20 07:42:36 +0900

    Fix: 결제서비스 로직 내부수정
    - 예약상태가 예약확정(CONFIRMED) 상태일때 결제내역생성

--- src/main/java/io/hhplus/concert/domain/payment/PaymentService.java
+++ src/main/java/io/hhplus/concert/domain/payment/PaymentService.java
@@ -1,5 +1,10 @@
 package io.hhplus.concert.domain.payment;
 
+import static io.hhplus.concert.interfaces.api.payment.PaymentErrorCode.*;
+
+import io.hhplus.concert.domain.reservation.Reservation;
+import io.hhplus.concert.domain.reservation.ReservationStatus;
+import io.hhplus.concert.interfaces.api.common.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +14,11 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
 
     public PaymentInfo.CreatePayment create(PaymentCommand.CreatePayment command) {
+        // 예약의 상태가 이미 결제처리까지 완료해서 확정(CONFIRMED)상태인지 확인
+        Reservation reservation = command.reservation();
+        if(reservation.getStatus() != ReservationStatus.CONFIRMED)
+            throw new BusinessException(NOT_VALID_STATUS_FOR_PAYMENT);
+
         Payment payment = paymentRepository.saveOrUpdate(Payment.of(command.reservation()));
         return PaymentInfo.CreatePayment.of(payment);
     }
