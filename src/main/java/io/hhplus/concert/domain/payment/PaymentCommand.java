5b5417702edb378436778445eb0e0c7acd74b535
eunkang-choi <dmsrkd1216@gmail.com>
2025-04-20 07:40:47 +0900

    Fix: PaymentCommand 유효성검사로직 추가

--- src/main/java/io/hhplus/concert/domain/payment/PaymentCommand.java
+++ src/main/java/io/hhplus/concert/domain/payment/PaymentCommand.java
@@ -1,10 +1,14 @@
 package io.hhplus.concert.domain.payment;
 
+import static io.hhplus.concert.interfaces.api.user.CommonErrorCode.*;
+
 import io.hhplus.concert.domain.reservation.Reservation;
+import io.hhplus.concert.interfaces.api.common.InvalidValidationException;
 
 public class PaymentCommand {
 	public record CreatePayment(Reservation reservation) {
 		public static CreatePayment of(Reservation reservation) {
+			if(reservation == null) throw new InvalidValidationException(NOT_NULLABLE);
 			return new CreatePayment(reservation);
 		}
 	}
