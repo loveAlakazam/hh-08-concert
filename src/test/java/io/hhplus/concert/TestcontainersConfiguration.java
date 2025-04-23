b136214bb5c23cb25d02af757a13d58d5aca6bbd
eunkang-choi <dmsrkd1216@gmail.com>
2025-04-23 18:25:26 +0900

    Chore: 테스트 컨테이너 sql로깅 옵션추가
    SQL로깅 옵션추가

--- src/test/java/io/hhplus/concert/TestcontainersConfiguration.java
+++ src/test/java/io/hhplus/concert/TestcontainersConfiguration.java
@@ -14,7 +14,7 @@ public class TestcontainersConfiguration {
 	public static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
 		.withDatabaseName("hhplus")
 			.withUsername("test")
-			.withPassword("test");;
+			.withPassword("test");
 
 	static  {
 		MYSQL_CONTAINER.start();
@@ -23,6 +23,8 @@ public class TestcontainersConfiguration {
 		System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
 		System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
 		System.setProperty("spring.jpa.hibernate.ddl-auto", "create");
+		System.setProperty("spring.jpa.show-sql", "true");
+		System.setProperty("spring.jpa.properties.hibernate.format-sql", "true");
 	}
 
 	@PreDestroy
