cc113ef8fdee0144c21f4be4bcd69416303787fb
eunkang-choi <dmsrkd1216@gmail.com>
2025-04-25 02:43:42 +0900

    Chore: 인프라테스트로 이관
    대기큐는 인프라레이어에 속하므로  관련 테스트케이스도 인프라로 이관

---
+++ src/test/java/io/hhplus/concert/infrastructure/queue/WaitingQueueUnitTest.java
@@ -0,0 +1,80 @@
+package io.hhplus.concert.infrastructure.queue;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.hhplus.concert.domain.token.WaitingQueue;
+import io.hhplus.concert.infrastructure.queue.InMemoryWaitingQueueImpl;
+
+public class WaitingQueueUnitTest {
+	private WaitingQueue queue;
+	@BeforeEach
+	void setUp() {
+		queue = new InMemoryWaitingQueueImpl();
+	}
+
+	@Test
+	void 큐에_삽입이_성공된다() {
+		// given
+		UUID uuid = UUID.randomUUID();
+
+		// when
+		queue.enqueue(uuid);
+
+		// then
+		assertThat(queue.size()).isEqualTo(1);
+		assertThat(queue.contains(uuid)).isTrue();
+		assertThat(queue.getPosition(uuid)).isEqualTo(1);
+	}
+	@Test
+	void peek은_맨첫번째_요소만을_조회할_수_있다() {
+		// given
+		UUID uuid1 = UUID.randomUUID();
+		UUID uuid2 = UUID.randomUUID();
+		queue.enqueue(uuid1);
+		queue.enqueue(uuid2);
+
+		// when
+		UUID result = queue.peek();
+
+		// then
+		assertThat(result).isNotEqualTo(uuid2);
+		assertThat(result).isEqualTo(uuid1);
+		assertThat(queue.size()).isEqualTo(2);
+	}
+	@Test
+	void dequeue는_맨첫번째_요소만을_큐에서_제거한다() {
+		// given
+		UUID uuid1 = UUID.randomUUID();
+		UUID uuid2 = UUID.randomUUID();
+		queue.enqueue(uuid1);
+		queue.enqueue(uuid2);
+
+		// when
+		UUID result = queue.dequeue();
+
+		// then
+		assertThat(result).isEqualTo(uuid1);
+		assertThat(result).isNotEqualTo(uuid2);
+		assertThat(queue.size()).isEqualTo(1);
+	}
+	@Test
+	void 큐의_모든원소들을_제거한다() {
+		// given
+		UUID uuid1 = UUID.randomUUID();
+		UUID uuid2 = UUID.randomUUID();
+		queue.enqueue(uuid1);
+		queue.enqueue(uuid2);
+
+		// when
+		queue.clear();
+
+		// then
+		assertThat(queue.size()).isEqualTo(0);
+
+	}
+}
