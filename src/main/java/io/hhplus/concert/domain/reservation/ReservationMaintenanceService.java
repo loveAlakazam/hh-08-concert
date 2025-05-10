03e0924db03aa3ce71b7081c92cde9e35aa57a8e
eunkang-choi <dmsrkd1216@gmail.com>
2025-05-10 10:29:40 +0900

    Feat: 예약취소 처리로 좌석상태변경으로 인해 쓰기전략 반영 & 관련 테스트코드 수정

--- src/main/java/io/hhplus/concert/domain/reservation/ReservationMaintenanceService.java
+++ src/main/java/io/hhplus/concert/domain/reservation/ReservationMaintenanceService.java
@@ -1,10 +1,17 @@
 package io.hhplus.concert.domain.reservation;
 
+import static io.hhplus.concert.domain.concert.ConcertService.*;
+import static io.hhplus.concert.domain.concert.ConcertService.CONCERT_SEAT_LIST_CACHE_TTL;
+
 import java.util.List;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.hhplus.concert.domain.concert.ConcertInfo;
 import io.hhplus.concert.domain.concert.ConcertSeat;
 import io.hhplus.concert.domain.concert.ConcertSeatRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +21,9 @@ import lombok.RequiredArgsConstructor;
 public class ReservationMaintenanceService {
 	private final ReservationRepository reservationRepository;
 	private final ConcertSeatRepository concertSeatRepository;
+
+	private final RedisTemplate<String, Object> redisTemplate;
+	private final ObjectMapper objectMapper;
 
 	/**
 	 * 임시예약 취소처리된 예약은 soft-delete 된다.
@@ -40,6 +50,14 @@ public class ReservationMaintenanceService {
 			// 만일 데이터가 없다면 해당 좌석의 상태값을 예약 가능으로 변경하고 저장한다
 			concertSeat.cancel();
 			concertSeatRepository.saveOrUpdate(concertSeat);
+
+			// 좌석의 상태가 변경되었으므로, 데이터베이스에서 좌석목록조회후에 캐시스토어에 바로 반영한다.
+			long concertId = concertSeat.getConcert().getId();
+			long concertDateId = concertSeat.getConcertDate().getId();
+			String cacheKey = CONCERT_SEAT_LIST_CACHE_KEY + "-" + "concert_id:" + concertId +"-" + "concert_date_id:" + concertDateId;
+
+			ConcertInfo.GetConcertSeatList concertSeats = concertSeatRepository.findConcertSeats(concertId, concertDateId);
+			redisTemplate.opsForValue().set(cacheKey, concertSeats, CONCERT_SEAT_LIST_CACHE_TTL);
 		}
 
 	}
