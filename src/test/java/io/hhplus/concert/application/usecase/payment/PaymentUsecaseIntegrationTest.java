c743d489e9114641514ecfdd8e11a6542e9603d2
eunkang-choi <dmsrkd1216@gmail.com>
2025-05-13 08:47:41 +0900

    Test: 결제후 매진이벤트가 발생했을때 당일 일간랭킹에 반영 통합테스트케이스 작성

--- src/test/java/io/hhplus/concert/application/usecase/payment/PaymentUsecaseIntegrationTest.java
+++ src/test/java/io/hhplus/concert/application/usecase/payment/PaymentUsecaseIntegrationTest.java
@@ -1,13 +1,20 @@
 package io.hhplus.concert.application.usecase.payment;
 
+import static io.hhplus.concert.domain.concert.Concert.*;
 import static io.hhplus.concert.domain.reservation.ReservationStatus.*;
+import static io.hhplus.concert.infrastructure.redis.ConcertRedisRepositoryImpl.*;
 import static io.hhplus.concert.interfaces.api.payment.PaymentErrorCode.*;
 import static io.hhplus.concert.interfaces.api.reservation.ReservationErrorCode.*;
 import static io.hhplus.concert.interfaces.api.user.CommonErrorCode.*;
 import static io.hhplus.concert.interfaces.api.user.UserErrorCode.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
@@ -22,6 +29,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
+import io.hhplus.concert.domain.concert.ConcertRedisRepository;
+import io.hhplus.concert.domain.support.CacheCleaner;
+import io.hhplus.concert.domain.support.CacheStore;
+import io.hhplus.concert.domain.support.SortedSetEntry;
 import io.hhplus.concert.infrastructure.containers.TestcontainersConfiguration;
 import io.hhplus.concert.domain.concert.Concert;
 import io.hhplus.concert.domain.concert.ConcertDate;
@@ -79,7 +90,10 @@ public class PaymentUsecaseIntegrationTest {
 	@Autowired private ConcertSeatRepository concertSeatRepository;
 	@Autowired private ReservationRepository reservationRepository;
 	@Autowired private PaymentRepository paymentRepository;
+	@Autowired private ConcertRedisRepository concertRedisRepository;
+	@Autowired private CacheStore cacheStore;
 
+	@Autowired private CacheCleaner cacheCleaner;
 	private static final Logger log = LoggerFactory.getLogger(PaymentUsecaseIntegrationTest.class);
 
 	User sampleUser;
@@ -118,6 +132,8 @@ public class PaymentUsecaseIntegrationTest {
 			)
 		);
 		sampleTemporaryReservation = reservationRepository.saveOrUpdate(info.reservation());
+
+		cacheCleaner.cleanAll();
 	}
 
 	@Order(1)
@@ -239,5 +255,65 @@ public class PaymentUsecaseIntegrationTest {
 		// 좌석은 점유한 상태로 그대로 있는지
 		assertNotNull(reservation.getConcertSeat());
 		assertFalse(reservation.getConcertSeat().isAvailable());
+	}
+	@Test
+	void 결제처리_성공이후_해당콘서트일정에서_매진이_발생했다면_인기랭킹순위_기록으로_레디스에_데이터가_적재된다() {
+		// given
+		long userId = sampleUser.getId();
+		long reservationId = sampleTemporaryReservation.getId();
+		long concertId = sampleConcert.getId();
+		long concertDateId = sampleConcertDate.getId();
+		LocalDate concertProgressDate = sampleConcertDate.getProgressDate();
+
+		// 예약좌석이 마지막 유효한 좌석이며 나머자 49개의 좌석은 모두 예약확정된 상태이다.
+		List<ConcertSeat> concertSeats = sampleConcertDate.getSeats();
+		List<User> users = IntStream.rangeClosed(1, 50)
+			.mapToObj(i->userRepository.save(User.of("테스트유저"+i))).toList();
+
+		for(int i=1; i<concertSeats.size(); i++) {
+			User user = users.get(i);
+			ConcertSeat concertSeat = concertSeats.get(i);
+			Reservation reservation = Reservation.of(user, sampleConcert, sampleConcertDate, concertSeat);
+			reservation.temporaryReserve();
+			reservation.confirm();
+			reservationRepository.saveOrUpdate(reservation);
+		}
+
+		// 임시예약상태
+		assertTrue(sampleTemporaryReservation.isTemporary());
+		// 유저포인트 15000원 충전 (좌석티켓가격보다 많은 포인트 보유)
+		sampleUserPoint.charge(15000);
+		userPointRepository.save(sampleUserPoint);
+
+		// when
+		PaymentResult.PayAndConfirm result = assertDoesNotThrow(
+			() -> paymentUsecase.payAndConfirm(PaymentCriteria.PayAndConfirm.of(userId, reservationId))
+		);
+
+		// then
+		// 결제 처리 완료됐음을 확인
+		assertNotNull(result.payment());
+		Payment payment = result.payment();
+		assertInstanceOf(Payment.class, payment);
+		assertNotNull(payment.getReservation());
+		// 예약정보 상태는 '확정상태'인지
+		Reservation reservation = payment.getReservation();
+		assertTrue(reservation.isConfirm());
+		assertEquals(CONFIRMED, reservation.getStatus());
+		// 좌석은 점유한 상태로 그대로 있는지
+		assertNotNull(reservation.getConcertSeat());
+		assertFalse(reservation.getConcertSeat().isAvailable());
+
+		// 예약한 콘서트일정의 상태가 매진상태인지 확인
+		sampleConcertDate = concertDateRepository.findConcertDateById(concertDateId);
+		assertFalse(sampleConcertDate.isAvailable(), "매진상태이므로 더이상 예약할 수 없는 상태이다");
+
+		// 매진이벤트가 발생하여 레디스저장소에 매진시점을 기록했는지 확인
+		Set<Object> members = concertRedisRepository.getDailyFamousConcertRanking();
+		String expectedMember = String.format("concert:%s:%s", concertId, concertProgressDate);
+		assertThat(members).contains(expectedMember);
+
+		String expectedKey = DAILY_FAMOUS_CONCERT_RANK_KEY + LocalDate.now(ZoneId.of(ASIA_TIMEZONE_ID));
+		assertThat(cacheStore.getExpire(expectedKey)).isGreaterThan(0);
 	}
 }
